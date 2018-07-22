b62c17bbaa53dd9515652c9ea5521f50c0832365
Joel Leibow <joel.leibow@workiva.com>
2018-07-22 16:31:36 -0700

    Redirect to provided redirect_url on uninstall

--- functions/hipchat/addon_uninstalled.ts
+++ functions/hipchat/addon_uninstalled.ts
@@ -5,13 +5,18 @@ export const addonUninstalled: Handler = (
   context: Context,
   done: Callback
 ) => {
-  // tslint:disable-next-line:no-console
-  console.log(event.body);
-  // tslint:disable-next-line:no-console
-  console.log(event.headers);
+  // const eventQueryStringParameters = {
+  //   installable_url: 'https://api.hipchat.com/v2/addon/2819299/installable/GamLUy6i2NWFbQL4O7I1',
+  //   redirect_url: 'https://workiva.hipchat.com/addons/?room=4678558'
+  // }
+
+  // The above info is within the event. We might need to do some cleaning up of things when uninstalled.
 
   const response = {
-    statusCode: 200
+    headers: {
+      location: event.queryStringParameters.redirect_url
+    },
+    statusCode: 302
   };
 
   done(null, response);
