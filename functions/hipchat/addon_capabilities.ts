f3e3c2cbf1fda1ed325ce45af273050827b5b2f7
Joel Leibow <joel.leibow@workiva.com>
2018-07-22 16:31:03 -0700

    Adjust capabilities and description

--- functions/hipchat/addon_capabilities.ts
+++ functions/hipchat/addon_capabilities.ts
@@ -9,7 +9,12 @@ export const addonCapabilities: Handler = (
     capabilities: {
       hipchatApiConsumer: {
         fromName: "App Frameworks Support Monitor",
-        scopes: ["send_notification"]
+        scopes: [
+          "send_message", // Send private one-on-one messages
+          "send_notification", // Send room notifications
+          "view_messages", // View messages from chat rooms and private chats you have access to
+          "view_room" // View room information and participants, but not history
+        ]
       },
       installable: {
         allowGlobal: false,
@@ -20,8 +25,7 @@ export const addonCapabilities: Handler = (
           "https://umoqgqh6k4.execute-api.us-east-1.amazonaws.com/dev/addon/uninstalled"
       }
     },
-    description:
-      "Listener and handler for messges within a Supoort Hipchat room",
+    description: "A supoort room message handler",
     key: "app-frameworks-support-monitor",
     links: {
       homepage:
@@ -38,7 +42,7 @@ export const addonCapabilities: Handler = (
       {
         authentication: "jwt",
         event: "room_message",
-        name: "Echo",
+        name: "Support room message handler",
         url:
           "https://umoqgqh6k4.execute-api.us-east-1.amazonaws.com/dev/support-messages"
       }
