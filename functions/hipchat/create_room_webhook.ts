b353c22fdbb704a1c8fba4ccf79f2aa5ffae10af
Joel Leibow <joel.leibow@workiva.com>
2018-07-22 08:37:22 -0700

    Handle request to add webhook to room

--- functions/hipchat/create_room_webhook.ts
+++ functions/hipchat/create_room_webhook.ts
@@ -1,18 +1,29 @@
-import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import { Handler } from "aws-lambda";
+import { randomBytes } from "crypto";
+import { config } from "dotenv";
 
-export const createRoomWebhook: Handler = (
-  event: APIGatewayEvent,
-  context: Context,
-  cb: Callback
-) => {
+import { RoomWebHookService } from "../../libs/hipchat/services/room";
+
+config();
+
+export const createRoomWebhook: Handler = async (event, context, callback) => {
+  const roomWebhookService = new RoomWebHookService(
+    event.pathParameters.roomId,
+    randomBytes(32).toString()
+  );
+
+  const roomWebhookServiceCreateResponse = await roomWebhookService.create({
+    event: "room_message",
+    name: "justATest",
+    url: "http://localhost:3000/support-messages"
+  });
+
+  const roomWebhook = await roomWebhookServiceCreateResponse.json();
+
   const response = {
-    body: JSON.stringify({
-      input: event,
-      message:
-        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!"
-    }),
-    statusCode: 200
+    body: JSON.stringify(roomWebhook),
+    statusCode: roomWebhook.status
   };
 
-  cb(null, response);
+  callback(null, response);
 };
