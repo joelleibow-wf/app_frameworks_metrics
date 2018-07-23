1d0a1a2f683683d8a6004dcf85c1c0eebd5fbf5d
Joel Leibow <joel.leibow@workiva.com>
2018-07-22 19:16:56 -0700

    Persist installs to DynamoDB

--- functions/hipchat/addon_installed.ts
+++ functions/hipchat/addon_installed.ts
@@ -1,18 +1,42 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import { AWSError, DynamoDB } from "aws-sdk";
+import { v1 } from "uuid";
 
 export const addonInstalled: Handler = (
   event: APIGatewayEvent,
   context: Context,
   done: Callback
 ) => {
-  // tslint:disable-next-line:no-console
-  console.log(event.body);
-  // tslint:disable-next-line:no-console
-  console.log(event.headers);
+  const data = JSON.parse(event.body);
 
-  const response = {
-    statusCode: 200
+  const installItem: DynamoDB.Types.PutItemInput = {
+    Item: {
+      capabilitiesUrl: { S: data.capabilitiesUrl },
+      groupId: { N: data.groupId.toString() },
+      id: { S: v1() },
+      installedOn: { S: new Date(Date.now()).toISOString() },
+      oauthId: { S: data.oauthId },
+      oauthSecret: { S: data.oauthSecret },
+      roomId: { N: data.roomId.toString() }
+    },
+    TableName: process.env.DYNAMODB_TABLE
   };
 
-  done(null, response);
+  const dyanmoDB = new DynamoDB();
+
+  dyanmoDB.putItem(installItem, (error: AWSError) => {
+    if (error) {
+      done(new Error(`Unable to save install w/ error: ${error}`));
+
+      return;
+    }
+
+    done(null, {
+      body: JSON.stringify(installItem.Item),
+      headers: {
+        "content-type": "application/json"
+      },
+      statusCode: 200
+    });
+  });
 };
