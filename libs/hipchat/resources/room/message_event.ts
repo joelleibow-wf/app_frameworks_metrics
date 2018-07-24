63cf90ef20956ea8cb217086c20b953097c88d7f
Joel Leibow <joel.leibow@workiva.com>
2018-07-23 17:55:21 -0700

    Room is nested within item

--- libs/hipchat/resources/room/message_event.ts
+++ libs/hipchat/resources/room/message_event.ts
@@ -5,8 +5,8 @@ export interface MessageEventResource {
   event: string;
   item: {
     message: MessageResource;
+    room: RoomResource;
   };
-  room: RoomResource;
   oauth_client_id: string;
   webhook_id: number;
 }
