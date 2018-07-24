d91fdaa37466ce97b88cca9e286ab1710a61c794
Joel Leibow <joel.leibow@workiva.com>
2018-07-23 17:45:48 -0700

    Rename export

--- libs/hipchat/resources/room/index.ts
+++ libs/hipchat/resources/room/index.ts
@@ -1,2 +1,5 @@
 export { HistoryResource as RoomHistoryResource } from "./history";
+export {
+  MessageEventResource as RoomMessageEventResource
+} from "./message_event";
 export { CreateWebHookResource as CreateRoomWebHookResource } from "./webhook";
