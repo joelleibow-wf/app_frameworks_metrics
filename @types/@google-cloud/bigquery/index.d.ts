2946d9aa555a9cf01526e7f93373ee27f3384e3f
Joel Leibow <joel.leibow@workiva.com>
2018-07-31 16:21:41 -0600

    Someday this might be worth completing. Not now.

---
+++ @types/@google-cloud/bigquery/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for @google-cloud/bigquery 1.3.0
+// Project: https://github.com/googleapis/nodejs-bigquery
+// TypeScript Version: 2.9.2
+
+declare module "@google-cloud/bigquery";
