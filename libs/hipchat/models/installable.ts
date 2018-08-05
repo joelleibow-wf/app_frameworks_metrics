3ee964a371c92a528e95f22004f3fba12c9c0212
Joel Leibow <joel.leibow@workiva.com>
2018-08-04 22:19:24 -0700

    More of that consistent naming

--- libs/hipchat/models/installable.ts
+++ libs/hipchat/models/installable.ts
@@ -1,6 +1,6 @@
-import { DynamoModel } from "../../framework";
+import { DynamoDBModel } from "../../framework";
 import { InstallableResource } from "../resources/installable";
 
-export class Installable extends DynamoModel<InstallableResource> {
+export class Installable extends DynamoDBModel<InstallableResource> {
   protected tableName = process.env.DYNAMODB_INSTALLS_TABLE;
 }
