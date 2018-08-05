1cc18d0438e0b3c86301dcff5a80d0bb9d959703
Joel Leibow <joel.leibow@workiva.com>
2018-08-04 22:21:08 -0700

    Jest config to enable TS

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "testURL": "http://localhost/",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "verbose": true,
+}
